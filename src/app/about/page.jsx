421aad5f607a7a757251dcf42fbdde3ff164ed46
roblebob <robert.koppisch@gmail.com>
2024-07-06 23:31:28 +0200

    @1:07:30 next.js images  done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" fill />
+      </div>
+    </div>
   );
-}
+};
 
 export default AboutPage;
