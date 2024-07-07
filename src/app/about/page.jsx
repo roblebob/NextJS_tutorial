62e83fb28e8aefe7d01860e7d5e5ec7b9a908281
roblebob <robert.koppisch@gmail.com>
2024-07-07 15:52:49 +0200

    @1:21:00 about-page done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,43 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.description}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.imgContainer}>
-        <Image src="https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" fill />
+        <Image
+          src="/about.png"
+          alt="About Image" 
+          className={styles.img}
+          fill
+        />
       </div>
     </div>
   );
