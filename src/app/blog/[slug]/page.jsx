0bd7aaef232457b9c0147d3c94cac8e42c0415ff
roblebob <robert.koppisch@gmail.com>
2024-07-09 15:43:56 +0200

    @1:48:00 design done

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,47 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+
+        <div className={styles.detail}>
+          <Image
+            className={styles.avatar}
+            src="https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            height={50}
+            width={50}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>John Doe</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>12.12.2021</span>
+          </div>
+        </div>
+
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
+          accusantium in, perspiciatis provident deserunt animi, nemo tenetur
+          amet accusamus vero reiciendis eius aperiam. Illum molestiae
+          praesentium atque numquam odit officiis?
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default SinglePostPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
   {
@@ -45,7 +46,16 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "/login" }} />
         )}
       </div>
-      <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+
+      <Image
+        src="/menu.png"
+        alt="menu"
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+        className={styles.menuButton}
+      />
+
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
