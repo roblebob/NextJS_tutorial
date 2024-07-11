c0e72e3b23d0e7081955f43442303c2ce6ebf5e8
roblebob <robert.koppisch@gmail.com>
2024-07-11 13:34:24 +0200

    @2:17:00 Navigation lecture done

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,10 @@
 import PostCard from "@/components/postCard/PostCard";
 import styles from "./blog.module.css";
 
-const BlogPage = () => {
+const BlogPage = ({params, searchParams}) => {
+
+  console.log(searchParams);
+
   return (
     <div className={styles.container}>
       <div className={styles.post}>
